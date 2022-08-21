459432a6c02e69122a301fe2627bcb11d3080c14
Jordan <jordantannen@gmail.com>
2022-08-20 21:47:17 -0400

    Change form to div

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,11 +3,11 @@ import React from "react";
 export default function Meme(){
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form--input" placeholder="Top Text"/>
                 <input type="text" className="form--input" placeholder="Bottom Text"/>
                 <button className="form--button">Get a new meme image 🖼</button>
-            </form>
+            </div>
         </main>
     )
 }
