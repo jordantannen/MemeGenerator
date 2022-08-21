11685eec3183452fe97a72d0ec129948ec2491a0
Jordan <jordantannen@gmail.com>
2022-08-20 20:30:36 -0400

    Add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header(){
+    return (
+        <nav className="header">
+            <h3>Meme Generator</h3>
+            <p>React Course - Project 3</p>
+        </nav>
+    )
+}
