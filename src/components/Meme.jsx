f4622d2c57dca468d9c3fe74bc9285efb2311c47
Jordan Tannenbaum <jordantannen@gmail.com>
2022-09-04 19:35:27 -0400

    Add random meme image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,13 +1,24 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme(){
+
+    const [memeImage, setMemeImage] = React.useState("");
+
+    function getMemeImage(){
+        const data = memesData.data.memes;
+        setMemeImage(data[Math.floor(Math.random() * data.length)].url)
+        console.log(1);
+    }
+
     return (
         <main>
             <div className="form">
                 <input type="text" className="form--input" placeholder="Top Text"/>
                 <input type="text" className="form--input" placeholder="Bottom Text"/>
-                <button className="form--button">Get a new meme image 🖼</button>
+                <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼</button>
             </div>
+            <img src={memeImage} className="meme--image" alt="" />
         </main>
     )
 }
